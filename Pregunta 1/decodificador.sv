995ab25d8e497a189b1dfb2a55bc9da63a472f2e
maarigonzalezz <ma.gonzalez@estudiantec.cr>
2025-08-16 18:25:10 -0600

    mejoras y avance

--- Pregunta 1/decodificador.sv
+++ Pregunta 1/decodificador.sv
@@ -1,11 +1,10 @@
 module decodificador ( input logic A, B, C, D, output logic s3, s2, s1, s0);
 
-	assign s3 = A;
-	
-	assign s2 = A ^ B;
-	
-	assign s1 = B ^ C;
-	
-	assign s0 = C ^ D;
+	always_comb begin
+        s3 = A;
+        s2 = A ^ B;
+        s1 = B ^ C;
+        s0 = C ^ D;
+    end
 
 endmodule 
